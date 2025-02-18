60754e9dce15051060f3b175eca743c33c77d1d4
Suraj Kumar <surajk90201@gmail.com>
2025-02-18 11:01:22 +0530

    making our code base more short

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-const passwordInput = document.querySelector('#password');  // Select the password input element
-
-const allPatternListElements = document.querySelectorAll('ul li[pattern]');  // Select all the list elements with the pattern attribute
-
-console.log(allPatternListElements);
 // Add an event listener to the password input element
-passwordInput.addEventListener('input', (event) => {
+password.addEventListener('input', (event) => {
     const inputValue = event.target.value;  // Get the value of the input element
-    allPatternListElements.forEach((listElement) => {
-        const pattern = listElement.getAttribute('pattern');  // Get the pattern attribute value
-        const regex = new RegExp(pattern);  // Create a regular expression object
-        const isMatch = regex.test(inputValue);  // Test the input value against the regular expression
-        if (isMatch) {
-            listElement.classList.add('match');  // Add the match class
-        } else {
-            listElement.classList.remove('match');  // Remove the match class
-        }
+    document.querySelectorAll("ul li[data-pattern]").forEach((listElement) => {
+        const regex = new RegExp(listElement.dataset.pattern);  // Create a regular expression object
+        (regex.test(inputValue)) ? listElement.classList.add("match") : listElement.classList.remove("match");
     });
-
 });
